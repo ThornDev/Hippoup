9cec749fdb30fffbc0e37fa22d1eb82dc79b3dec
ThornDev <jiemianwang@gmail.com>
2017-02-20 22:14:07 +0800

    add file

---
+++ Hippoup/TVBMediator.swift
@@ -0,0 +1,13 @@
+//
+//  TVBMediator.swift
+//  Hippoup
+//
+//  Created by 王绵杰 on 2017/2/20.
+//  Copyright © 2017年 王绵杰. All rights reserved.
+//
+
+import UIKit
+
+class TVBMediator: NSObject {
+
+}
